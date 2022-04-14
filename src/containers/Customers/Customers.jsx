fcc10c4cadf8545597a7fe41c848c58a9e115525
Sasha Ajin <sashaajin@icloud.com>
2022-04-14 15:27:31 +0300

    create customers table

--- src/containers/Customers/Customers.jsx
+++ src/containers/Customers/Customers.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
 
 const Customers = () => {
-    return (
-        <div>
-            customers
-        </div>
-    )
-}
+  return (
+    <div className="container table-customers-vehicles">
+      <div className="uk-overflow-auto">
+        <table className="uk-table  uk-table-divider">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Vip</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Sasha Ajintarev</td>
+              <td>sashaajin@gmail.com</td>
+              <td>+</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>Sasha</td>
+              <td>sashaajin@gmail.com</td>
+              <td>+</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
 
-export default Customers
+export default Customers;
